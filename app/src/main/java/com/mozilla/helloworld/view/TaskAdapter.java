43bd2db68c68f1cc67fb3b1458cee8c872bb30dc
Antoine PESTEL-ROPARS <antoine.pestel.ropars@proton.me>
2024-01-31 11:35:34 +0100

    Tweaks

--- app/src/main/java/com/mozilla/helloworld/view/TaskAdapter.java
+++ app/src/main/java/com/mozilla/helloworld/view/TaskAdapter.java
@@ -17,22 +17,18 @@ import java.util.List;
 import java.util.Objects;
 
 public class TaskAdapter extends ArrayAdapter<Task> {
-
     private final int resourceId;
-
     public TaskAdapter(@NonNull Context context, int resource, @NonNull List<Task> objects) {
         super(context, resource, objects);
         this.resourceId = resource;
     }
-
     @NonNull
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-
-        @NonNull Task task = Objects.requireNonNull(getItem(position));
         if ( convertView == null ) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(this.resourceId, parent, false);
         }
+        @NonNull Task task = Objects.requireNonNull(getItem(position));
         TextView taskTitle = convertView.findViewById(R.id.task_title_text_view);
         taskTitle.setText(task.getTitle());
         return convertView;
--- app/src/main/java/com/mozilla/helloworld/view/TaskFragment.java
+++ app/src/main/java/com/mozilla/helloworld/view/TaskFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -21,41 +20,37 @@ public class TaskFragment extends Fragment {
     public TaskFragment() {
         // Required empty public constructor
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_task, container, false);
     }
-
-    private ListView listView;
-
     private TaskAdapter taskAdapter;
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        listView = view.findViewById(R.id.tasks_list_view);
+        ((MainActivity) requireActivity()).addTask("Git gud");
+
         FloatingActionButton floatingActionButton = view.findViewById(R.id.add_task_button);
-        ((MainActivity) requireActivity()).addTask("Android Course");
         floatingActionButton.setOnClickListener(v -> openDialogAddTask());
-        taskAdapter = new TaskAdapter(
-                requireActivity(),
-                R.layout.task_item,
-                ((MainActivity) requireActivity()).getTasks());
+
+        ListView listView = view.findViewById(R.id.tasks_list_view);
         listView.setAdapter(taskAdapter);
         listView.setOnItemClickListener((parent, view1, position, id) -> {
             //TODO
         });
-    }
 
+        taskAdapter = new TaskAdapter(
+                requireActivity(),
+                R.layout.task_item,
+                ((MainActivity) requireActivity()).getTasks()
+        );
+    }
     private void openDialogAddTask() {
         EditText editText = new EditText(getContext());
         AlertDialog dialog = new AlertDialog
